70993a1d8a1bd5822b981cace56a3e245bdaa64c
Iamneo <techadmin@iamneo.ai>
2023-09-21 08:18:14 +0000

    Backup Commit

--- ec2_instance/ec2.tf
+++ ec2_instance/ec2.tf
@@ -9,5 +9,9 @@ provider "aws"{
     region = "ap-southeast-1"
 }
 resource "aws_instance" "problem1" {
-    ami = ""
+    ami = "ami-0db42e3a06a0b3e13"
+    instance_type = "t2.micro"
+}
+output "public_ip" {
+  value       = aws
 }
